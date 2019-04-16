054b47ed962fa3616f5df494d061f166078d3bd1
smeerws <hello@smeerws.at>
2019-04-16 14:57:08 +0200

    experimenting with wall tweak

--- Assets/TheGame/Scripts/BallController.cs
+++ Assets/TheGame/Scripts/BallController.cs
@@ -2,6 +2,8 @@
 
 public class BallController : MonoBehaviour
 {
+    private enum Direction { InDirection, AgainstDirection};
+
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
 
@@ -11,10 +13,10 @@ public class BallController : MonoBehaviour
     private float direction; // +1 From Bottom to Top; -1 From Top to Bottom
     private bool dirToggelt;
 
-    public float xPush = 2.0f;
+    public float xPush = 10.0f;
     public float zPush = 15.0f;
     public float speed = 0.10f;
-    public float randomFactor = 0.2f;
+    public float randomFactor = 0.5f;
 
 
     private void Start ()
@@ -30,32 +32,58 @@ public class BallController : MonoBehaviour
     {
         if (hasStarted)
         {
-            rb.velocity = new Vector3 (xPush*direction, yPos, zPush);
+            rb.velocity = new Vector3 (xPush*direction*speed, yPos, zPush);
             hasStarted = false;
         }
     }
 
     public void OnCollisionEnter (Collision collision)
     {
-        if (collision.gameObject.name == "WallPingTop" || collision.gameObject.name == "WallPongBottom")
+        if (collision.gameObject.tag == "Wall")
         {
-            ToogleDirection();
+            if(collision.gameObject.name == "WallPingTop" || collision.gameObject.name == "WallPongBottom")
+            {
+                BounceOf(Direction.AgainstDirection);
+            }
+            else
+            {
+                BounceOf(Direction.InDirection);
+            }
+            
         }
-        else if (collision.gameObject.name == "PingPlayer")
+        else if (collision.gameObject.name == "PingPlayer" || collision.gameObject.name == "PongEnemy")
         {
-            ToogleDirection();
+            BounceOf(Direction.AgainstDirection);
         }
+    }
+
+    private void BounceOf(Direction direction)
+    {
+        Debug.Log("Before dirchange: " + this.direction);
+        if (direction == Direction.AgainstDirection)
+        {
+            ToogleDirection(); 
+            Debug.Log("against Direction");
+            Debug.Log("After dirchange: " + this.direction);
+        }
+
+        AddTweak();
+        Debug.Log("Added Tweak: " + this.direction);
     }
 
     public void ToogleDirection()
     {
-        Vector3 velocityTweak = new Vector3
-           (Random.Range(0.0f, randomFactor),
-            yPos,
-            Random.Range(0.0f, randomFactor));
-
         direction = direction * -1.0f;
-        rb.velocity = new Vector3(xPush * direction, yPos, zPush)+ velocityTweak;
-        rb.velocity += velocityTweak;
+    }
+
+    public void AddTweak()
+    {
+        Vector3 velocityTweak = new Vector3
+                                   (Random.Range(0.0f, randomFactor),
+                                    0.0f,
+                                    Random.Range(0.0f, randomFactor));
+        rb.velocity = new Vector3(xPush * direction, yPos, zPush ) + velocityTweak;
+        //rb.velocity += velocityTweak;
+        Debug.Log("add tewak");
     }
 }
--- Assets/TheGame/Scripts/PlayerController.cs
+++ Assets/TheGame/Scripts/PlayerController.cs
@@ -26,19 +26,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Debug.Log("Update");
-        Debug.Log("ActionSTate Teleport " + SteamVR_Actions._default.Teleport.GetActiveBinding(SteamVR_Input_Sources.Any));
-        Debug.Log("ActionSTate movepaddle " + SteamVR_Actions._default.MovePaddle.GetActiveBinding(SteamVR_Input_Sources.Any));
-        Debug.Log("ActionSTate Teleport " + SteamVR_Actions._default.Teleport.GetActiveBinding(SteamVR_Input_Sources.Any));
-        Debug.Log("ActionSTate movepaddle active " + SteamVR_Actions._default.MovePaddle.GetActive(SteamVR_Input_Sources.Any));
-        //Debug.Log(SteamVR_Actions._default.Teleport.GetActive);
-        Debug.Log(SteamVR_Actions._default.MovePaddle.GetAxis(SteamVR_Input_Sources.LeftHand));
-   
-        Debug.Log("Update");
         var leftHoriz = movePaddleAction.GetAxis(SteamVR_Input_Sources.LeftHand);
         var rightHoriz = movePaddleAction.GetAxis(SteamVR_Input_Sources.RightHand);
-        Debug.Log("Update " + leftHoriz.ToString() + ", " + rightHoriz.ToString());
-        //Debug.Log(sensitivity + "linear mapping " + linearMapping.value.ToString());
 
         if (leftHoriz.x < -sensitivity || rightHoriz.x < -sensitivity)
         {
@@ -46,7 +35,6 @@ public class PlayerController : MonoBehaviour
             if (linearMapping.value < maxMapping)
             {
                 linearMapping.value += mapStep;
-                Debug.Log("move Left: " + linearMapping.value);
             }
         }
         else if (leftHoriz.x > sensitivity || rightHoriz.x > sensitivity)
@@ -54,11 +42,9 @@ public class PlayerController : MonoBehaviour
             if (linearMapping.value > minMapping)
             {
                 linearMapping.value -= mapStep;
-                Debug.Log(linearMapping.value);
             }
         }
 
-        Debug.Log(linearMapping.value);
         //thisRb.velocity = new Vector3(0,0,linearMapping.value);
         thisTransform.position = new Vector3(thisTransform.position.x, thisTransform.position.y, Mathf.Clamp(-linearMapping.value,minMapping,maxMapping));
     }
